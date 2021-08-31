b1772dafbde0f1cebcabbb28386284bf1b396632
Mikko Nummelin <mnummeli@gmail.com>
2021-08-31 20:22:28 +0300

    Initial commit.

---
+++ src/main/java/fi/tslespoo/kattikahvila/KattikahvilaApplication.java
@@ -0,0 +1,13 @@
+package fi.tslespoo.kattikahvila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KattikahvilaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KattikahvilaApplication.class, args);
+    }
+
+}
